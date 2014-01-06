73ac23a2243bbba4ab7794976dcb590dd34a9a5a
Eric Allard <eric.allard0104@gmail.com>
2014-01-06 10:42:26 +0100

    add backbone

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,16 @@
+var Measure = require('mongoose').model('measures');
+
 exports.index = function(req, res){
   res.render('index', { title: 'ScaleWS' });
+};
+
+exports.measures = function (req, res) {
+      //console.log('list', req.body);
+      Measure.find({}, function (err, result) {
+        if (!err) {
+          res.send(result);
+        } else {
+          res.send(errMsg(err));
+        }
+      });
 };
