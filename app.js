7d174fda287de4f073745d06741242f1cbd3d1ef
Eric Allard <eric.allard0104@gmail.com>
2014-01-26 17:01:28 +0100

    Revert "early implementation of socket.iobind communication way"
    This reverts commit fa5f6824d35afe5a3c48b77b9801ff89909b6bee.

--- app.js
+++ app.js
@@ -14,26 +14,13 @@ var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-var io = io.listen(server);
-var measures = require('./models/measure')(mongoose, io.sockets);
+var sockets = io.listen(server).sockets;
+sockets.on('connection', function (socket) {
+		socket.emit('news', "hello you !")
+	  socket.broadcast.emit('news', "new user connected");
+});
 
-io.sockets.on('connection', function (socket) {
-  
-  /**
-   * measures:read
-   *
-   * called when we .fetch() our collection
-   * in the client-side router
-   */
-
-  socket.on('measures:read', function (data, callback) {
-
-    callback(null, measures.find());
-  });
-
-
-});  
-
+var measures = require('./models/measure')(mongoose, sockets);
 
 // Routes
 var scale = require('./routes/cgi-bin')(measures),
@@ -42,5 +29,5 @@ var scale = require('./routes/cgi-bin')(measures),
 // Client
 routes.init(app, model);
 // Scale
-app.get('/cgi-bin/:page', scale);
+app.post('/cgi-bin/:page', scale);
 app.post('/cgi-bin/:version/:page', scale);
