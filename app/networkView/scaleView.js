2c3e67f0c5536c42b4170d5d70fc4d9041a77d0d
Fedonono <arnaud.babol@gmail.com>
2014-04-11 23:55:30 +0200

    Refactor routes scaleView (server view)

--- app/networkView/scaleView.js
+++ app/networkView/scaleView.js
@@ -1,44 +1,19 @@
 module.exports = function(app, Backbone){
+  return Backbone.ServerView.extend({
+    initialize:  function () {
+      app.post('/cgi-bin/once', this.once);
+      app.get('/cgi-bin/session', this.session);
+      app.post('/cgi-bin/maint', this.maint);
+      app.post('/cgi-bin/measure', this.measure);
+      app.post('/cgi-bin/v2/measure', this.measurev2);
+      app.post('/cgi-bin/v2/weather', this.weather);
 
-  return Backbone.ServerView.extend({    
-
-    initialize :  function () {
-      app.post('/cgi-bin/:page', this.router);
-      app.post('/cgi-bin/:version/:page', this.router);
       if(!this.collection){
         console.log("ScaleView can't access to the collection");
       }
     },
 
-    router : function() {
-      return function (req, res) {
-        var page = req.params.page,
-        version = req.params.version || '';
-
-        if (version !== '') version += '_';
-
-        if (version === '' && page === 'index') {
-          // avoid infinite loop
-          fail();
-        }else{
-          var func = this[version + page]
-          if(!func){
-            console.log('Invalid request: ' + version + page );
-            fail();
-          }else{
-            res.setHeader('Content-Type', 'text/plain');
-            func(req,res,view);
-          }
-        }
-
-        function fail() {
-          res.send(404);
-        }
-      }
-    },
-
-    addMeasure : function(value, type, date){
-
+    saveMeasure: function(value, type, date){
       if (typeof(date) === 'undefined')
         date = new Date();
 
@@ -49,38 +24,39 @@ module.exports = function(app, Backbone){
       });
     },
 
-    
-
-    maint : function(req, res){
+    maint: function(req, res){
       res.end('{"status":0}');
     },
 
-    measure : function(req, res){
+    measure: function(req, res){
       // measure.create(Math.floor(100*Math.random()), Math.floor(10000*Math.random()));
       var measureScale = JSON.parse(req.body.measuregrps).measuregrps;
+      console.log(measureScale);
       for (var m in measureScale) {
         for (var n in measureScale[m].measures) {
           var tmp = measureScale[m].measures[n];
-          console.log(tmp.type+" value : "+ tmp.value);
-          this.addMeasure(tmp.value, tmp.type);
+          console.log(tmp.type+" value: "+ tmp.value);
+          this.saveMeasure(tmp.value, tmp.type);
           // 0
-          // 1 value : 84216 -- weight
-          // 16 value : 522 -- BIA ou IBE => ce n'est pas la qualité de l'air -- Bioelectrical Impedance Analysis - Unit: body fat % plus le taux de graisse est élevé, plus la résistance à laquelle se heurte le signal électrique est grande.
+          // 1 value: 84216 -- weight
+          // 16 value: 522 -- BIA ou IBE => ce n'est pas la qualité de l'air -- Bioelectrical Impedance Analysis - Unit: body fat % plus le taux de graisse est élevé, plus la résistance à laquelle se heurte le signal électrique est grande.
           // 1
-          // 11 value : 93 -- Heart
+          // 11 value: 93 -- Heart
         }
       }
       res.end('{"status":0,"body":{"updatetime":'+Math.round(new Date().getTime()/1000)+'}}');
     },
 
-    once : function(req, res){
+    once: function(req, res){
       res.end('{"status":0,"body":{"once":"00d37f9b-0d05b260"}}');
     },
 
-    session : function(req, res){
+    session: function(req, res){
+      console.log(this.session())
+      res.end('{"status":0}');
       if (req.body.action === 'new') {
-        console.log("[DATA] Battery : "+req.body.batterylvl);
-        this.addMeasure(req.body.batterylvl, 42);
+        console.log("[DATA] Battery: "+req.body.batterylvl);
+        this.saveMeasure(req.body.batterylvl, 42);
         res.end('{"status":0,"body":{"sessionid":"4079-f5f75535-2f5ea8eb","sp":{"users":[{"id":2507298,"sn":"FED","wt":86.812,"ht":2,"agt":21.3,"sx":0,"fm":3,"cr":1385155190,"att":0}]},"ind":{"lg":"fr_FR","imt":1,"stp":1,"f":0,"g":98113},"syp":{"utc":'+Math.round(new Date().getTime()/1000)+'},"ctp":{"goff":3600,"dst":1396141200,"ngoff":7200}}}');
       }
       if (req.body.action === 'delete') {
@@ -89,7 +65,7 @@ module.exports = function(app, Backbone){
       }
     },
 
-    v2_measure : function(req, res){
+    measurev2: function(req, res){
       var measureScale = JSON.parse(req.body.measuregrps);
       console.log(measureScale);
       for (var m in measureScale) {
@@ -98,12 +74,16 @@ module.exports = function(app, Backbone){
           var value = Math.round(tmp.data.mantissa*Math.pow(10, tmp.data.exponent)*100)/100;
           var type = tmp.type;
           var time = Date(measureScale[m].meastime);
-          this.addMeasure(value, type, time);
+          this.saveMeasure(value, type, time);
           // type 12 => temperature
           // type 35 => 655 => qualité air, taux CO2 en ppm
         }
       }
       res.end('{"status":0}');
+    },
+
+    weather: function(req, res) {
+      res.end('{{"status":0,"body":{"series":[]}}');
     }
 
   });
