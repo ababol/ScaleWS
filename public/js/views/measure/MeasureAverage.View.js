f109c4e517c6a3eb6140c2bae71cdd0e8a76101c
Eric Allard <eric.allard0104@gmail.com>
2014-04-13 19:20:24 +0200

    fix averageView issues with better implementation

--- public/js/views/measure/MeasureAverage.View.js
+++ public/js/views/measure/MeasureAverage.View.js
@@ -18,35 +18,39 @@ define([
       this.average = $("#"+this.el.attr('id') + ' p.average');
     },
     addOne: function(measure) {
-      this.last.html(Math.round(measure.get("value")));
-      this.updateView();
+      if(this.rightType(measure.get("type")))
+        this.updateView();
     },
     remove: function(measure) {
-      this.updateView();
-      console.log("remove");
+      if(this.rightType(measure.get("type")))
+        this.updateView();
     },
     update: function(measure) {
-      this.updateView();
-      console.log("update");
+      if(this.rightType(measure.get("type")))
+        this.updateView();
     },
     updateView: function(){
-      var lastValue = _.reduce(this.collection.models, function(min, c){
-          if(c.get("type") != this.type)
-            return min;
-          if(min == null)
+      var lastValue = _.reduce(this.collection.where({"type" : this.type}), function(min, c){
+          if(min === null)
             return c;
           else if(min.get("date") > c.get("date"))
             return min;
           else
             return c;
         }, null, this);
-      var averageValue =  _.reduce(this.collection.models, function(num, c){
-          if(c.get("type") != this.type)
-            return num;
-		  return c.get("value") + num;
+      
+      var averageValue =  _.reduce(this.collection.where({"type" : this.type}), function(num, c){
+		      return c.get("value") + num;
         }, 0, this) / this.collection.where({"type" : this.type}).length; 
-      this.average.html("Average: " + Math.floor(averageValue));
-      this.last.html("Last: " + Math.floor(lastValue.get("value")));
+      
+      if(averageValue)
+        this.average.html("Average: " + Math.floor(averageValue));
+      else
+        this.average.html("No value");
+      if(lastValue)
+        this.last.html("Last: " + Math.floor(lastValue.get("value")));
+      else
+        this.last("No value");
     }
   });
 }); 
